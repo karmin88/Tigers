54dcfa8b99a17dedd8a72d8ceffddb590240c8b5
Lykeios1221 <derricksaw123@gmail.com>
2023-12-11 01:28:43 +0800

    setup login and sign up, along with login required validation and session mechanism

--- api.py
+++ api.py
@@ -1,19 +1,65 @@
-from flask import render_template, flash, session
+from functools import wraps
+
+from flask import render_template, flash, request, session, redirect, url_for
 from app import app
-from app import mysql
+from database import find_user, insert_user, add_user
+
+
+@app.before_request
+def permanent_session():
+    session.permanent = True
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('email', None):
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/')
 def index():
-    login = False
-    return render_template('public-dashboard.html', login=login)
+    return render_template('public-dashboard.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        role = request.form.get('role')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember = request.form.get('remember')
+        user = find_user(email)
+        if user:
+            if remember:
+                session['email'] = email
+            else:
+                session.pop('email', None)
+            flash(f"Welcome back, user {user['username']}!", 'success')
+            return render_template('public-dashboard.html')
     return render_template('login.html')
 
 
-@app.route('/sign_up')
+@app.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        retype_password = request.form.get('retype-password')
+        role = request.form.get('role')
+        if password != retype_password:
+            flash('Passwords do not match.', 'danger')
+            return render_template('signup.html')
+        user = find_user(email)
+        if user:
+            flash('Email already registered.', 'danger')
+        else:
+            if add_user(email, username, password, role):
+                flash('User successfully registered.', 'success')
+                return render_template('login.html')
+            else:
+                flash('Unexpected error has occurred. Please try again.', 'danger')
     return render_template('signup.html')
