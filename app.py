4d39413de24e19298526bf3c6a7974772417c76e
Lykeios1221 <derricksaw123@gmail.com>
2023-12-11 01:27:40 +0800

    modify so that sql queries return dict, and set session timeout

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ from flask_mysqldb import MySQL
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 app.secret_key = secrets.token_hex(32)
+app.config['PERMANENT_SESSION_LIFETIME'] = 120
 
 # Database connection setup
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_DB'] = 'skig3013_project'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
