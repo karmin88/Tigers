a89e5902f2ca56ff236c00aee30695abbc78b9c5
Lykeios1221 <derricksaw123@gmail.com>
2023-12-13 01:45:47 +0800

    add more dynamic behavior and implement submit using ajax

--- static/js/add-note.js
+++ static/js/add-note.js
@@ -17,26 +17,32 @@ const dropzoneConfig = {
                 done("Only one PDF file is allowed per section.");
             } else {
                 const fileUrl = URL.createObjectURL(file)
-                $(this['element']).parents('.card-body').find('.embed-responsive-item').attr('src', fileUrl)
+                $(this['element']).closest('.card-body').find('.embed-responsive-item').attr('src', fileUrl)
             }
         } else {
             done();
         }
     },
+
     removedfile: function (file) {
         if (file.type === 'application/pdf' && !this.files.some(f => f.type === 'application/pdf')) {
-            const iframeBody = $(this['element']).parents('.card-body').find('.embed-responsive-item').contents().find('body');
-            iframeBody.html('<h1>PDF not found or has been removed</h1>')
-            iframeBody.css('background-color', '#efefef')
+            const iframeBody = $(this['element']).closest('.card-body').find('.embed-responsive-item').contents().find('body');
+            iframeBody.html('    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">\n    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">\n<div class="container-fluid h-100 d-flex justify-content-center align-items-center flex-column my-5">\n    <i class="bi bi-exclamation-triangle-fill text-danger h4"></i>\n    <h1 class="text-center ">PDF tidak ditemui atau telah dipadamkan</h1>\n</div>')
+            iframeBody.css('background-color', 'white')
         }
         if (file.previewElement != null && file.previewElement.parentNode != null) {
             file.previewElement.parentNode.removeChild(file.previewElement);
         }
         return this._updateMaxFilesReachedClass();
     },
-    dictDefaultMessage: `<i class="bi bi-cloud-arrow-up-fill h1"></i>
-                                <br>Click or drop files here to upload
-                                <br>Only 1 PDF and up to 5 images are allowed per section (<5Mb)`,
+
+    dictDefaultMessage: `
+    <div class=" text-muted">
+        <i class="bi bi-cloud-arrow-up-fill h1"></i>
+        <br class="text-muted">Klik atau seret dan laspankan fail di sini untuk dimuatnaik.
+        <br>Hanya 1 PDF dan sehingga 5 imej yang dibenarkan untuk setiap bahagian (kurang dari 5Mb)
+    </div>`,
+
     init: function () {
         this.on('addedfile', function (file) {
             $('.dz-progress').addClass('d-none');
@@ -52,18 +58,24 @@ const dropzoneConfig = {
     }
 }
 
-function addSection() {
-    const newSection = $('#section-form-template').contents().clone();
-    $('#section-container').append(newSection);
+function addSection(skipScroll = false) {
+    const newSection = $('#section-form-template').contents().clone(true, true);
+    const container = $('#section-container');
+    container.append(newSection);
     const dropzone = newSection.find('.dropzone')
     dropzone.dropzone(dropzoneConfig);
-}
 
-$(document).ready(function () {
+    newSection.find('.close').on('click', function (evt) {
+        const currentForm = $(this).closest('form');
+        const prevForm = currentForm.prev();
+        currentForm.remove()
+        window.scrollTo({
+            top: prevForm.get(0).getBoundingClientRect().top + window.pageYOffset - 25,
+            behavior: 'smooth',
+        })
+    });
 
-    addSection()
-
-    $('.collapsible').on('click', function () {
+    newSection.find('.collapsible').on('click', function () {
         const collapseIcon = $(this).children('.bi-collapsible');
         if (collapseIcon.hasClass('bi-arrows-angle-contract')) {
             collapseIcon.addClass('bi-arrows-angle-expand');
@@ -74,8 +86,54 @@ $(document).ready(function () {
         }
         $(this).siblings('.card-body').slideToggle('fast');
     });
+    if (skipScroll) {
+        window.scrollTo({
+            top: newSection.get(1).getBoundingClientRect().top + window.pageYOffset - 25,
+            behavior: 'smooth',
+        })
+    }
+}
+
+$(document).ready(function () {
+
+    addSection()
 
     $('#add-section').on('click', function (evt) {
-        addSection()
+        addSection(true)
+    });
+
+    $('#submit-note').on('click', function (evt) {
+
+        const forms = $('#section-container .section-form');
+        const selectedChap = $('#selected-chap');
+        if (selectedChap.val() === null) {
+            selectedChap[0].setCustomValidity('Please select a chapter.')
+            console.log(selectedChap.val())
+            selectedChap[0].reportValidity()
+            return;
+        } else {
+            selectedChap[0].setCustomValidity('')
+            selectedChap[0].reportValidity()
+        }
+        if (forms.toArray().some(form => !form.reportValidity())) return;
+        let counter = 1;
+        let allSectionFormData = new FormData();
+        allSectionFormData.append('total_section', forms.length)
+        allSectionFormData.append('chapter', selectedChap.val())
+        forms.each(function () {
+            for (value of new FormData(this).values()) {
+                allSectionFormData.append(`s${counter}_data`, value);
+            }
+            const dropzone = Dropzone.forElement($(this).find('.dropzone')[0])
+            dropzone.files.forEach(file => allSectionFormData.append(`s${counter}_files`, file))
+            counter++;
+        });
+        $.ajax({
+            type: 'POST',
+            url: '/teacher_note',
+            data: allSectionFormData,
+            contentType: false,
+            processData: false
+        }).then(window.location.reload());
     });
 })
